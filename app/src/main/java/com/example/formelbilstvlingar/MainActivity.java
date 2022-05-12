1d05ae6c15cc331196ad3fcaf3aada6fe88966a6
c21alest <alexsanderstrom@outlook.com>
2022-05-12 14:04:40 +0200

    Skapade en instans av recycler view i main activity

--- app/src/main/java/com/example/formelbilstvlingar/MainActivity.java
+++ app/src/main/java/com/example/formelbilstvlingar/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.formelbilstvlingar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
+
+    private final String JSON_URL = "url";
+    private final String JSON_FILE = "file.json";
+
+    RecyclerView myRecyclerView;
+    RecyclerView.Adapter myAdapter;
+    RecyclerView.LayoutManager myLayoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new JsonFile(this, this).execute(JSON_FILE);
+
+    }
+
+    @Override
+    public void onPostExecute(String json) {
+
+        // Skapar en recycler view instans
+        myRecyclerView = findViewById(R.id.recycler_view);
+        // Optimerings parameter
+        myRecyclerView.setHasFixedSize(true);
+        myLayoutManager = new LinearLayoutManager(this);
+        myRecyclerView.setLayoutManager(myLayoutManager);
+        myAdapter = new MainAdapter();
+        myRecyclerView.setAdapter(myAdapter);
     }
 }
