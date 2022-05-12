d362b296e2141e043d555cfc50b2e3d54cf3c9d5
c21alest <alexsanderstrom@outlook.com>
2022-05-12 14:10:10 +0200

    Skapade en recycler view adapter

---
+++ app/src/main/java/com/example/formelbilstvlingar/MainAdapter.java
@@ -0,0 +1,42 @@
+package com.example.formelbilstvlingar;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainAdapter extends RecyclerView.Adapter<MainAdapter.ViewHolder> {
+
+    @NonNull
+    @Override
+    public MainAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Skapar en ny view för list_items som används för att presentera innehåller i recycler view
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_items, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView textD;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            // Kopplar variabel mot id i en layout
+            textD = itemView.findViewById(R.id.display_text);
+        }
+    }
+}
